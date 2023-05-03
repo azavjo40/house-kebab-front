616a6520f2b162528b0b788584b9d98a408f5a5a
Azam Suiev <asufiev@rightandabove.com>
2023-05-03 16:16:18 +0200

    added apiUrl

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    apiUrl: 'http://localhost:1337',
+    // apiUrl: 'http://localhost:1337',
+    apiUrl: 'https://house-kebab-back.herokuapp.com',
   },
 }
 
