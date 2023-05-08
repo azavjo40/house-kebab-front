11750e998cbc0d5be05c2c4d47b5ed695947d6db
Azam Sufiev <asufiev@rightandabove.com>
2023-05-08 02:24:22 +0200

    removed url image

--- src/containers/banner/index.tsx
+++ src/containers/banner/index.tsx
@@ -15,18 +15,10 @@ export const Banner = () => {
   };
   return (
     <div className="w-full h-[150px] md:h-[200px] relative">
-      {header?.banner ? (
-        <img className="w-full h-[90%]" src={process.env.apiUrl + header?.banner || ""} alt="house Bnner" />
-      ) : (
-        ""
-      )}
+      {header?.banner ? <img className="w-full h-[90%]" src={header?.banner || ""} alt="house Bnner" /> : ""}
       <div className="w-[62px] h-[62px] md:w-[72px] md:h-[72px] absolute left-10 md:left-20 -bottom-2 bg-slate-100 rounded-sm flex justify-center items-center">
         {header?.logo ? (
-          <img
-            className="w-[62px] h-[52px] md:w-[72px] md:h-[62px]"
-            src={process.env.apiUrl + header?.logo || ""}
-            alt="house Logo"
-          />
+          <img className="w-[62px] h-[52px] md:w-[72px] md:h-[62px]" src={header?.logo || ""} alt="house Logo" />
         ) : (
           ""
         )}
