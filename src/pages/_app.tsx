55570bb8b0353d903672e34908f7e8d2ad6a8b65
Azam Sufiev <asufiev@rightandabove.com>
2023-06-10 13:32:45 +0200

    fixed heroku error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { SocketContextProvider } from "../stores/socket";
 import { DenseAppBar } from "../components/Navbar/components/Dense-app-bar";
 import { MobileAppBar } from "../components/Navbar/components/Mobile-app-bar";
 // import { Footer } from "../components/Footer/";
-import { AlertMessage } from "../components/Alert-message";
+// import { AlertMessage } from "../components/Alert-message";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { asPath } = useRouter();
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <MobileAppBar />
         <Component {...pageProps} />
         {/* {!(asPath.includes("/basket") || asPath.includes("/admin")) && <Footer />} */}
-        <AlertMessage />
+        {/* <AlertMessage /> */}
       </SocketContextProvider>
     </GeneralContextProvider>
   );
