317ca47265803e336e4f5b571e47966a2b087b74
Azam Sufiev <asufiev@rightandabove.com>
2023-06-10 13:30:11 +0200

    fixed heroku error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,10 @@ import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { GeneralContextProvider } from "../stores/general";
 import { SocketContextProvider } from "../stores/socket";
-import { DenseAppBar } from "../components/Navbar/components/Dense-app-bar";
-import { MobileAppBar } from "../components/Navbar/components/Mobile-app-bar";
-import { Footer } from "../components/Footer";
-import { AlertMessage } from "../components/Alert-message";
+import { DenseAppBar } from "../components/Navbar/components/Dense-app-bar/index";
+import { MobileAppBar } from "../components/Navbar/components/Mobile-app-bar/index";
+import { Footer } from "../components/Footer/index";
+import { AlertMessage } from "../components/Alert-message/index";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { asPath } = useRouter();
